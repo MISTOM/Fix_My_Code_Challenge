2e3c17db1ea46f85ad0fe0f9814adb859f200289
MISTOM <kigardetom2001@gmail.com>
2022-02-14 23:08:29 +0300

    fix square class defination

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""class Square """
+class square():
+    
+    width = 0
+    height = 0
+
+    
+    def __init__(self, *args, **kwargs):
+        """Initializes the square"""
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def permiter_of_my_square(self):
+        """Perimeter of the square"""
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """String representation of the square"""
+        return "{}/{}".format(self.width, self.height)
+
+if __name__ == "__main__":
+
+    s = square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
