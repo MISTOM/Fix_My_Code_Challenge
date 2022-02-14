3d80f3c0431262b903e8c8b896fc6ee011a609a8
MISTOM <kigardetom2001@gmail.com>
2022-02-14 23:23:21 +0300

    Modify square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
+""" Class squares """
 
-"""class Square """
-class square():
-    
+
+class Square:
+    """Represents a rectangle with equal sides."""
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
-        """Initializes the square"""
+        """ Initializes new square. """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
-        return self.width * self.width
+        """ Computes the area of the square. """
+        return self.width * self.height
 
     def permiter_of_my_square(self):
-        """Perimeter of the square"""
+        """ Computes the perimeter of the square. """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """String representation of the square"""
+        """ Computes the string format of the square. """
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+if __name__ == "__main__":
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.permiter_of_my_square())
